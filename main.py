cbf3b9a616952c881b03d4ad94966729bb9dd69b
Franm99 <fran.moreno.se@gmail.com>
2025-09-07 20:38:57 +0200

    Added log files to record tracks

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import os
 import time
 import tkinter as tk
 import tkinter.font as tkf
@@ -6,6 +8,8 @@ import keyboardlayout.tkinter as klt
 
 from colour import Color
 from collections import defaultdict
+from datetime import datetime
+from pathlib import Path
 from pynput import keyboard
 
 
@@ -82,7 +86,20 @@ class KeyboardDisplay:
         
 
 class KeyboardTracker:
-    def __init__(self):
+    def __init__(self, logger_path: Path = None):
+        self._logger_path = logger_path if logger_path else Path("logs")
+        
+        self._timestamp = datetime.now()
+        self._date = self._timestamp.strftime(format="%d-%m-%Y")
+        self._hour = self._timestamp.strftime(format="%H:%M:%S")
+        
+        self.log_data: dict = {
+            "date": self._date,
+            "duration": "",
+            "total_count": 0,
+            "keylog": {},
+        }
+        
         self._total_count: int = 0
         self._key_count: defaultdict = defaultdict(int)
         
@@ -92,6 +109,24 @@ class KeyboardTracker:
     def run(self):
         with keyboard.Listener(on_press=self._on_press, on_release=self._on_release) as listener:
             listener.join()
+            
+    def save(self):
+        self.log_data["keylog"] = dict(self._key_count)
+        
+        end_time = datetime.now()
+        duration_s = (end_time - self._timestamp).seconds
+        h, m, s = self._seconds_to_hours_string(duration_s)
+        
+        self.log_data["total_count"] = sum(list(self._key_count.values()))
+        self.log_data["duration"] = [h, m, round(s, 4)]
+        
+        log_file_name = f"record_{self._timestamp.strftime("%m%d%Y_%H%M%S")}.json"
+        
+        if not os.path.exists(self._logger_path):
+            os.makedirs(self._logger_path)
+        
+        with open(self._logger_path / log_file_name, 'w', encoding="utf-8") as f:
+            json.dump(self.log_data, f, indent=4)
             
     def _on_press(self, key, _injected):
         if hasattr(key, "char"):
@@ -109,15 +144,11 @@ class KeyboardTracker:
             
             if self._esc_count == self._esc_count_to_end:
                 print("Stopping keyboard tracker.")
-                self._compute_stats()
                 self._display_stats()
                 return False  # Stops listener
         else:
-            self._esc_count = 0
-            
-    def _compute_stats(self):
-        self._total_count = sum(list(self._key_count.values()))
-        
+            self._esc_count = 0        
+                
     def _display_stats(self):
         print(f"TOTAL KEYS PRESSED: {self._total_count}")
         print("--------------")
@@ -127,6 +158,14 @@ class KeyboardTracker:
         for key, count in key_count_sorted:
             print(f"{key} -> Pressed {count} times.")
             
+    @staticmethod
+    def _seconds_to_hours_string(total_seconds: float) -> tuple:
+        hours = total_seconds // 3600
+        mins = (total_seconds % 3600) // 60
+        seconds = (total_seconds % 3600) % 60
+        
+        return hours, mins, seconds
+
     @property
     def key_count(self) -> dict:
         return self._key_count
@@ -136,6 +175,7 @@ if __name__ == '__main__':
     tracker = KeyboardTracker()
     ts = time.time()
     tracker.run()
+    tracker.save()
     print("=== FINISHED ===")
     tf = time.time() - ts
     print(f"TOTAL TIME: {tf:.2f}")
